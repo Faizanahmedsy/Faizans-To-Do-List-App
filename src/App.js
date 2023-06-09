38738bcca09924ee7e3ee8b6cbd11adf4e42747f
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-06-09 12:13:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import { TodoWrapper } from "./components/TodoWrapper";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
